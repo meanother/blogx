15c28e4aa547b75881a126186db642334d4539cd
arty <juicehq@yandex.ru>
2020-06-16 23:16:37 +0300

    collectstatic

--- config/config/settings.py
+++ config/config/settings.py
@@ -1,8 +1,8 @@
 
 try:
-    from .settings_prod import *
+    from .settings_dev import *
 except ImportError:
-    pass
+    from .settings_prod import *
 
 
 
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'blog'), os.path.join(BASE_DIR, 'media'), os.path.join(BASE_DIR, 'users'),)
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
