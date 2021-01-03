edff53f687eea3e3c016dbb5249f2184656ac4ad
meanother <juicehq@yandex.ru>
2021-01-03 13:42:17 +0300

    Лайтовые правки во фронтенд части

--- config/blog/models.py
+++ config/blog/models.py
@@ -9,7 +9,8 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    header_image = models.ImageField(default='default_header.png', upload_to='post_pics')
+    # header_image = models.ImageField(default='default_header.png', upload_to='post_pics')
+    header_image = models.ImageField(default='default.jpeg', upload_to='post_pics')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
